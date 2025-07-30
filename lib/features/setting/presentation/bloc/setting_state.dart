04146f051c3dfa8dc95907569e7c07833dde8d34
Yasin-A10 <yasinzizi8383@gmail.com>
2025-07-30 14:55:49 +0330

    ...

--- lib/features/setting/presentation/bloc/setting_state.dart
+++ lib/features/setting/presentation/bloc/setting_state.dart
@@ -13,12 +13,10 @@ final class SettingLoading extends SettingState {
   const SettingLoading(bool isDarkMode) : super(isDarkMode);
 }
 
-/// حالت موفق پس از تغییر تم
 final class SettingLoaded extends SettingState {
   const SettingLoaded(bool isDarkMode) : super(isDarkMode);
 }
 
-/// حالت خطا
 final class SettingError extends SettingState {
   final String message;
   const SettingError(this.message, bool isDarkMode) : super(isDarkMode);
